81c2d8b152310942c83e50a4f11ef8a88fc69b67
ViniciusOnofreBaptista <74966304+ViniciusOnofreBaptista@users.noreply.github.com>
2022-03-10 16:23:10 -0300

    first commit

---
+++ knowledge/src/main/java/com/br/knowledge/KnowledgeApplication.java
@@ -0,0 +1,22 @@
+package com.br.knowledge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class KnowledgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowledgeApplication.class, args);
+	}
+	
+	@GetMapping("/teste")
+	public String testando() {
+		return "Teste Ok";
+	}
+
+}
